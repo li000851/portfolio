7459e1df825bb47dade32cacd4a4a7723c48ce73
Siwei Li <li000851@algonquinlive.com>
2023-11-29 19:41:55 -0500

    add contact page

--- images/Portfolio - Siwei Li_files/main.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.navbar-links');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
-  burger.classList.toggle('toggle'); 
-});
